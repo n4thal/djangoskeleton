a5d21dc032b1f0f438d4e595a150d13416efa188
nth <martin.schlander@hotmail.com>
2020-08-03 18:32:32 +0200

    added projects to admin panel

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Project
+
 
 # Register your models here.
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'technology', 'publication_date')
+    list_filter = ('technology', 'publication_date')
+    search_fields = ('title', 'description')
+
+
+admin.site.register(Project, ProjectAdmin)
--- projects/models.py
+++ projects/models.py
@@ -7,3 +7,11 @@ class Project(models.Model):
     description = models.TextField()
     technology = models.CharField(max_length=20)
     image = models.FilePathField(path='static/img')
+    publication_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        from django.urls import reverse
+        return reverse("projects_detail", kwargs={"technology": str(self.technology)})
