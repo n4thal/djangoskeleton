5aa8be3c2291c7322d49125c99f2f9c2d2fdebf3
nth <martin.schlander@hotmail.com>
2020-08-03 18:45:14 +0200

    added testpicture and added a summary for projects

--- projects/models.py
+++ projects/models.py
@@ -4,6 +4,7 @@ from django.db import models
 # Create your models here.
 class Project(models.Model):
     title = models.CharField(max_length=255)
+    summary = models.TextField(max_length=255)
     description = models.TextField()
     technology = models.CharField(max_length=20)
     image = models.FilePathField(path='static/img')
