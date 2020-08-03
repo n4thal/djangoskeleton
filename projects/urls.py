9919d1aae8297b3fee4d33264fa74e24031a486c
nth <martin.schlander@hotmail.com>
2020-08-03 18:13:56 +0200

    added link to projects app

--- projects/urls.py
+++ projects/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.projects_index, name="projects_index"),
+    path('', views.projects_index, name="projects"),
     path('<int:pk>/', views.projects_detail, name="projects_detail"),
 ]
