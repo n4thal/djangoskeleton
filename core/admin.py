4a9e1f567a6f4b8d5c48d6b95bd47662b3872d0f
nth <martin.schlander@hotmail.com>
2020-08-06 10:05:51 +0200

    added website metainfo and author to admin panel and db

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Author, MetaInfo
+
 
 # Register your models here.
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    search_fields = ('name', 'postcode', 'city', 'mail', 'phone')
+
+
+class MetaInfoAdmin(admin.ModelAdmin):
+    list_display = ('description', 'keywords')
+    list_filter = ('description', 'keywords')
+    search_fields = ('description', 'keywords')
+
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(MetaInfo, MetaInfoAdmin)
--- core/models.py
+++ core/models.py
@@ -2,6 +2,24 @@ from django.db import models
 
 
 # Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+    street = models.CharField(max_length=255)
+    postcode = models.IntegerField()
+    city = models.CharField(max_length=31)
+    mail = models.EmailField()
+    phone = models.CharField(max_length=15)
+    hook = models.CharField(max_length=511)
+    description = models.CharField(max_length=2000)
+    history = models.CharField(max_length=2000)
+    call_to_action = models.CharField(max_length=127)
+
+
+class MetaInfo(models.Model):
+    description = models.CharField(max_length=511)
+    keywords = models.CharField(max_length=2000)
+
+
 class Core(models.Model):
     pass
 
