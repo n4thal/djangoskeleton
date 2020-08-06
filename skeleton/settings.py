8f415bdcaac2f7446edb26d16a2eda89e1409096
nth <martin.schlander@hotmail.com>
2020-08-06 18:20:06 +0200

    fixed the static file path

--- skeleton/settings.py
+++ skeleton/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # Location of static files
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, ''), ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
