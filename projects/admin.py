532ba3795c1b7e79651d5b9edc0261658052e128
nth <martin.schlander@hotmail.com>
2020-08-22 17:54:22 +0200

    added wysiwyg editor 'Summernote' to admin project panel

--- projects/admin.py
+++ projects/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import Project
 
 
 # Register your models here.
-class ProjectAdmin(admin.ModelAdmin):
+class ProjectAdmin(SummernoteModelAdmin):
     list_display = ('title', 'technology', 'publication_date')
     list_filter = ('technology', 'publication_date')
     search_fields = ('title', 'description')
